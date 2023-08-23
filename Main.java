0af34e3cdf0ec21976cfb0821b88d7c6dba33b31
Simarjeet Singh <simarjeetss529@gmail.com>
2023-08-23 11:49:29 +0530

    first commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Student{
+    int[] rno = new int[5];
+    String[] name = new String[5];
+    float[] marks = new float[5];
+}
